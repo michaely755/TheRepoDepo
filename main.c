53c0dd03800a42b5a07bff64c0007f9f7bf115b6
michaely755 <55446528+michaely755@users.noreply.github.com>
2023-04-21 23:35:45 -0400

    Add files via upload
    lab 10

--- main.c
+++ main.c
@@ -1,39 +1,90 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void bubble_sort(int arr[], int n){
-    int temp;
-    for(int i = 0; i < n-1; i++){
-        int num_swaps = 0;
-        for(int j = 0; j < n-i-1; j++){
-            if(arr[j] > arr[j+1]){
-                temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-                num_swaps++;
-            }
-        }
-        printf("pass %d: %d\n", i+1, num_swaps);
-    }
+#define ALPHABET_SIZE 26
+
+struct TrieNode {
+  struct TrieNode *children[ALPHABET_SIZE];
+  int count;
+};
+
+struct Trie {
+  struct TrieNode *root;
+};
+
+struct TrieNode *createNode() {
+  struct TrieNode *node = (struct TrieNode *)malloc(sizeof(struct TrieNode));
+  node->count = 0;
+  for (int i = 0; i < ALPHABET_SIZE; i++) {
+    node->children[i] = NULL;
+  }
+  return node;
 }
 
-int main()
-{
-    int arr[9] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
-    printf("before swap\n");
-    for(int i = 0; i < 9; i++){
-        printf("%d ", arr[i]);
+void insert(struct Trie **ppTrie, char *word) {
+  struct Trie *pTrie = *ppTrie;
+  if (pTrie == NULL) {
+    pTrie = (struct Trie *)malloc(sizeof(struct Trie));
+    pTrie->root = createNode();
+    *ppTrie = pTrie;
+  }
+
+  struct TrieNode *current = pTrie->root;
+  for (int i = 0; i < strlen(word); i++) {
+    char c = tolower(word[i]);
+    if (current->children[c - 'a'] == NULL) {
+      current->children[c - 'a'] = createNode();
     }
-    printf("\n");
+    current = current->children[c - 'a'];
+  }
+  current->count++;
+}
 
-    bubble_sort(arr, 9);
-
-    printf("after swap\n");
-    for(int i = 0; i < 9; i++){
-        printf("%d ", arr[i]);
+int numberOfOccurrences(struct Trie *pTrie, char *word) {
+  struct TrieNode *current = pTrie->root;
+  for (int i = 0; i < strlen(word); i++) {
+    char c = tolower(word[i]);
+    if (current->children[c - 'a'] == NULL) {
+      return 0;
     }
-    printf("\n");
+    current = current->children[c - 'a'];
+  }
+  return current->count;
+}
 
+void deallocateNode(struct TrieNode *node) {
+  for (int i = 0; i < ALPHABET_SIZE; i++) {
+    if (node->children[i] != NULL) {
+      deallocateNode(node->children[i]);
+    }
+  }
+  free(node);
+}
 
-    return 0;
+struct Trie *deallocateTrie(struct Trie *pTrie) {
+  if (pTrie != NULL) {
+    deallocateNode(pTrie->root);
+    free(pTrie);
+    pTrie = NULL;
+  }
+  return pTrie;
+}
+
+int main(void) {
+  struct Trie *trie = NULL;
+  // read the number of the words in the dictionary
+  //  parse line  by line, and insert each word to the trie data structure
+  char *pWords[] = {"notaword", "ucf", "no", "note", "corg"};
+
+  
+
+  for (int i = 0; i < 5; i++) {
+    printf("\t%s : %d\n", pWords[i], numberOfOccurances(&trie, pWords[i]));
+  }
+  trie = deallocateTrie(&trie);
+  if (trie != NULL)
+    printf("There is an error in this program\n");
+  return 0;
 }
